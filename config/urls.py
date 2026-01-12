0af84bca2c8d3759847ea0140baed44b6fbaa16e
Shamir Towsif <shamir.towsif@protonmail.com>
2026-01-12 06:53:15 -0600

    update files

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for jholok project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home),
 ]
